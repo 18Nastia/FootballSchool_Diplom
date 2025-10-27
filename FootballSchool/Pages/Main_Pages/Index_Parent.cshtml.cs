0fa4d1302c6f65768bf78ab4ff16519a3a4d0059
Терпугова Анастасия <Nastiaterp18022007@gmail.com>
2025-10-27 15:32:57 +0300

    Все страницы реализованы до момента к отчету лаб 7

---
+++ FootballSchool/Pages/Main_Pages/Index_Parent.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FootballSchool.Pages.Main_Pages
+{
+    public class Index_ParentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
